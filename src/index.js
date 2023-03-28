4855dc2985bb90d22929d688cc590f768d11e8f3
Filipe Biasoli <filipe.biasoli@gmail.com>
2023-03-28 16:13:20 -0300

    conexão da api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('teste');
+require('dotenv/config');
+
+// server connection
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// listening api
+app.listen(process.env.PORT, () => {
+  console.log(`API listening at port ${process.env.PORT}`);
+});
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    response: 'success',
+  });
+});
+
+// routes
